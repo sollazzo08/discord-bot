7abbbb9b78c978b6fd3ad85794a9b14d142bf46d
Sollazzo08 <msollazzo08@gmail.com>
2025-02-14 20:13:15 -0500

    started work on movie-show parser

---
+++ internal/commands/movies-shows.go
@@ -0,0 +1,43 @@
+package commands
+
+import (
+
+	"github.com/bwmarrin/discordgo"
+)
+
+
+
+const CHANNELID = "786329251338911775"
+
+// We want to return a general MessageData Struct, general enough so that AI can extract the correct detail
+//WE NEED A SUB STRUCT FOR GETTING THE AUTHOR DETAILS OF A EMOJI REACTION ON A MESSAGE
+type MessageData struct {
+	ID        string // Message ID
+	AuthorID  string // User ID of the sender
+	Username  string // Username of the sender
+	Content   string // The actual message text
+	Timestamp string // Message timestamp
+	EmojiReactionDetails struct {
+		AuthorID  string // User ID of the sender
+		Username  string // Username of the sender
+		EmojiDetail string //Emoji details, should be a number rating
+
+	}
+}
+
+// We need to first read the channel called movies-shows. We will need to grab the channel ID
+// Grab all users in the channel and collect them in an array. Grab User Names. We are going to build out user profiles with their movies
+
+// Fetching the data logic
+// There is a rate limit of 100 api calls for discord
+// Using the ChannelMessages function from Discord I can pull up to 100 message per call.
+// When I make my first parse on the channel. I can start from the first message by getting the message ID.
+// We can use this as our afterID parameter, so all 100 messages after the first will be parsed.
+// We then need to track the id of the last mesg we parsed so we can start the parse over again at that mesg.
+// Repeat until we reach the end, end can be determined by a response of 0 from the ChannelMessages call.
+
+func fetchChannelData (s *discordgo.Session, m *discordgo.MessageCreate, cfg string) {
+
+	//allChannelMessage, err := s.ChannelMessages(CHANNELID, 100,, "","")
+}
+
